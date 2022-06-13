6dd488cfec7061593a5fe67687a42246a0b71c89
ciceromachado <cicero.machado@mercadolivre.com>
2022-06-13 16:44:18 -0300

    Update payment_v2.js

--- payment_v2.js
+++ payment_v2.js
@@ -1,6 +1,6 @@
 LoadCheckoutPaymentContext(function (Checkout, PaymentOptions) {
-  var Custom = PaymentOptions.Transparent.CardPayment({
-    id: "mypayments_redirect_mercado_pago_cicero",
+  var Credit = PaymentOptions.Transparent.CardPayment({
+    id: "mypayments_credito_mercado_pago_cicero",
 
     fields: {
       card_holder_birth_date: true,
@@ -25,5 +25,32 @@ LoadCheckoutPaymentContext(function (Checkout, PaymentOptions) {
     },
   });
 
-  Checkout.addPaymentOption(Custom);
+ var Debit = PaymentOptions.Transparent.CardPayment({
+    id: "mypayments_debito_mercado_pago_cicero",
+
+    fields: {
+      card_holder_birth_date: true,
+    },
+
+    onLoad: function () {
+      // Do something after the script loads.
+      // Example: generate a token.
+    },
+
+    onDataChange: Checkout.utils.throttle(function () {
+      // Do something when the input form data changes.
+      // Data changed is already available on `Checkout.getData()`.
+      // Example: update credit card installments when the order value changes.
+    }, 700),
+
+    onSubmit: function (callback) {
+      // Do something when user submits the payment.
+      callback({
+        success: true, // Or false.
+      });
+    },
+  });
+
+  Checkout.addPaymentOption(Credit);
+  Checkout.addPaymentOption(Debit);
 });
